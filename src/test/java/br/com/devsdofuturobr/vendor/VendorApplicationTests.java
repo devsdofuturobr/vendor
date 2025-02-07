49161819aaea0df8145c5f317a3835d967257562
Madson Silva <maddytec@gmail.com>
2025-02-07 23:26:42 +0000

    First commit

---
+++ src/test/java/br/com/devsdofuturobr/vendor/VendorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsdofuturobr.vendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
